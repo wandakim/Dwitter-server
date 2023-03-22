4e49eeea764d2059c8d005ee4803349ffd7d4ce6
wandakim <nwd1583@naver.com>
2023-03-22 10:25:10 +0900

    Tweet APIs:CRUD

--- router/tweets.js
+++ router/tweets.js
@@ -3,7 +3,7 @@ import 'express-async-errors';
 
 const router = express.Router();
 
-const tweets = [
+let tweets = [
   {
     id: '1',
     text: 'I am Maker!',
@@ -26,14 +26,52 @@ const tweets = [
 router.get('/', (req, res, next) => {
   const username = req.query.username;
   const data = username
-    ? tweets.filter((t) => t.username === username)
+    ? tweets.filter((tweet) => tweet.username === username)
     : tweets;
   res.status(200).json(data);
 });
 
 // GET /tweets/:id
+router.get('/:id', (req, res, next) => {
+  const id = req.params.id;
+  const tweet = tweets.find((tweet) => tweet.id === id);
+  tweet
+    ? res.status(200).json(tweet)
+    : res.status(404).json({ message: `Tweet id(${id}) not found` });
+});
+
 // POST /tweets
+
+router.post('/', (req, res, next) => {
+  const { text, name, username } = req.body;
+  const tweet = {
+    id: Date.now().toString(),
+    text,
+    creratedAt: new Date(),
+    name,
+    username,
+  };
+  tweets = [tweet, ...tweets];
+  res.status(201).json(tweet);
+});
+
 // PUT /tweets/:id
+router.put('/:id', (req, res, next) => {
+  const id = req.params.id;
+  const text = req.body.text;
+  const tweet = tweets.find((tweet) => tweet.id === id);
+  if (tweet) {
+    tweet.text = text;
+    res.status(200).json(tweet);
+  } else {
+    res.statud(404).json({ message: `Tweet id(${id}) not found` });
+  }
+});
 // DELETE /tweets/:id
+router.delete('/:id', (req, res, next) => {
+  const id = req.params.id;
+  tweets = tweets.filter((tweet) => tweet.id !== id);
+  res.sendStatus(204);
+});
 
 export default router;
