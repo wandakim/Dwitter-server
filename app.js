775fcb6ede015d6322ffc9fb9948922989e04a82
wandakim <nwd1583@naver.com>
2023-04-06 14:20:38 +0900

    failed do updated with ready to part

--- app.js
+++ app.js
@@ -11,9 +11,14 @@ import { sequelize } from './db/database.js';
 
 const app = express();
 
+const corsOption = {
+  origin: config.cors.allowedOrigin,
+  optionsSuccessStatus: 200,
+};
+
 app.use(express.json());
 app.use(helmet());
-app.use(cors());
+app.use(cors(corsOptioin));
 app.use(morgan('tiny'));
 
 app.use('/tweets', tweetsRouter);
@@ -29,7 +34,8 @@ app.use((error, req, res, next) => {
 });
 
 sequelize.sync().then(() => {
-  const server = app.listen(config.host.port);
+  console.log(`Server is started... ${new Date()}`);
+  const server = app.listen(config.port);
   initSocket(server);
 });
 // sequal
